e39d8a3ab69870d1d5a59969d8f608df3e04fd96
zohaib-eng <zohaibdon41@gmail.com>
2026-03-27 20:45:01 +0500

    Feature-update-config files.

---
+++ basic-crud-example/src/main/java/com/example/basic_crud_example/BasicCrudExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.basic_crud_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicCrudExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicCrudExampleApplication.class, args);
+	}
+
+}
---
+++ basic-crud-example/src/test/java/com/example/basic_crud_example/BasicCrudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basic_crud_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicCrudExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
